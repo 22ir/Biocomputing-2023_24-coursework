d6a320485068a6035066b523ca66552f9c6c6529
Ieva Ratkeviciute <156655168+22ir@users.noreply.github.com>
2024-04-14 17:34:37 +0300

    Add files via upload

---
+++ Dog_dna_code.py
@@ -0,0 +1,35 @@
+import run_clustalw from my_modules
+
+# performing multiple sequence alignments
+input_file_path1 = "./combined_breeds_first_10.fa"
+stdout, stderr = run_clustalw(input_file_path1)
+
+input_file_path2 = "./combined_breeds_2nd_set.fa"
+stdout, stderr = run_clustalw(input_file_path2)
+
+input_file_path3 = "./combined_breeds_3rd_set.fa"
+stdout, stderr = run_clustalw(input_file_path3)
+
+input_file_path4 = "./combined_breeds_4th_set.fa"
+stdout, stderr = run_clustalw(input_file_path4)
+
+input_file_path5 = "./combined_breeds_5th_set.fa"
+stdout, stderr = run_clustalw(input_file_path5)
+
+input_file_path6 = "./combined_breeds_6th_set.fa"
+stdout, stderr = run_clustalw(input_file_path6)
+
+input_file_path7 = "./combined_breeds_7th_set.fa"
+stdout, stderr = run_clustalw(input_file_path7)
+
+input_file_path8 = "./combined_breeds_8th_set.fa"
+stdout, stderr = run_clustalw(input_file_path8)
+
+input_file_path9 = "./combined_breeds_9th_set.fa"
+stdout, stderr = run_clustalw(input_file_path9)
+
+input_file_path10 = "./combined_breeds_10th_set.fa"
+stdout, stderr = run_clustalw(input_file_path10)
+
+input_file_path11 = "./combined_breeds_11th_set.fa"
+stdout, stderr = run_clustalw(input_file_path11)
