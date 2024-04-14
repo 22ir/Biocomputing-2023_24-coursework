fcde75fe01f8a471e4434b5ff6e40bd48aa199d7
Ieva Ratkeviciute <156655168+22ir@users.noreply.github.com>
2024-04-14 20:20:34 +0300

    Update Dog_dna_code.py

--- Dog_dna_code.py
+++ Dog_dna_code.py
@@ -1,53 +1,56 @@
-import run_clustalw from my_modules
-import draw_phylogenetic_tree from my_modules
+import Dog_dna_modules 
+
+# Create smaller .fa files containing 10 sequences (myster.fa seq + 9 other dog breed sequences)
+
+Dog_dna_modules.mystery_and_9breeds("mystery.fa", "dog_breeds.fa", "dog_breeds_subset", 9)
 
 # performing multiple sequence alignments
-input_file_path1 = "./combined_breeds_first_10.fa"
-stdout, stderr = run_clustalw(input_file_path1)
+input_file_path1 = "./combined_breeds_subset_1.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path1)
 
-input_file_path2 = "./combined_breeds_2nd_set.fa"
-stdout, stderr = run_clustalw(input_file_path2)
+input_file_path2 = "./combined_breeds_subset_2.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path2)
 
-input_file_path3 = "./combined_breeds_3rd_set.fa"
-stdout, stderr = run_clustalw(input_file_path3)
+input_file_path3 = "./combined_breeds_subset_3.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path3)
 
-input_file_path4 = "./combined_breeds_4th_set.fa"
-stdout, stderr = run_clustalw(input_file_path4)
+input_file_path4 = "./combined_breeds_subset_4.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path4)
 
-input_file_path5 = "./combined_breeds_5th_set.fa"
-stdout, stderr = run_clustalw(input_file_path5)
+input_file_path5 = "./combined_breeds_subset_5.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path5)
 
-input_file_path6 = "./combined_breeds_6th_set.fa"
-stdout, stderr = run_clustalw(input_file_path6)
+input_file_path6 = "./combined_breeds_subset_6.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path6)
 
-input_file_path7 = "./combined_breeds_7th_set.fa"
-stdout, stderr = run_clustalw(input_file_path7)
+input_file_path7 = "./combined_breeds_subset_7.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path7)
 
-input_file_path8 = "./combined_breeds_8th_set.fa"
-stdout, stderr = run_clustalw(input_file_path8)
+input_file_path8 = "./combined_breeds_subset_8.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path8)
 
-input_file_path9 = "./combined_breeds_9th_set.fa"
-stdout, stderr = run_clustalw(input_file_path9)
+input_file_path9 = "./combined_breeds_subset_9.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path9)
 
-input_file_path10 = "./combined_breeds_10th_set.fa"
-stdout, stderr = run_clustalw(input_file_path10)
+input_file_path10 = "./combined_breeds_subset_10.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path10)
 
-input_file_path11 = "./combined_breeds_11th_set.fa"
-stdout, stderr = run_clustalw(input_file_path11)
+input_file_path11 = "./combined_breeds_subset_11.fa"
+stdout, stderr = Dog_dna_modules.run_clustalw(input_file_path11)
 
 
 # Drawing a phylogenetic trees for each .dnd file produced after running multiple sequence alignments on 11 sets of .fa files
-file_path1 = "./combined_breeds_first_10.dnd"
-file_path2 = "./combined_breeds_2nd_set.dnd"
-file_path3 = "./combined_breeds_3rd_set.dnd"
-file_path4 = "./combined_breeds_4th_set.dnd"
-file_path5 = "./combined_breeds_5th_set.dnd"
-file_path6 = "./combined_breeds_6th_set.dnd"
-file_path7 = "./combined_breeds_7th_set.dnd"
-file_path8 = "./combined_breeds_8th_set.dnd"
-file_path9 = "./combined_breeds_9th_set.dnd"
-file_path10 = "./combined_breeds_10th_set.dnd"
-file_path11 = "./combined_breeds_11th_set.dnd"
+file_path1 = "./combined_breeds_subset_1.dnd"
+file_path2 = "./combined_breeds_subset_2.dnd"
+file_path3 = "./combined_breeds_subset_3.dnd"
+file_path4 = "./combined_breeds_subset_4.dnd"
+file_path5 = "./combined_breeds_subset_5.dnd"
+file_path6 = "./combined_breeds_subset_6.dnd"
+file_path7 = "./combined_breeds_subset_7.dnd"
+file_path8 = "./combined_breeds_subset_8.dnd"
+file_path9 = "./combined_breeds_subset_9.dnd"
+file_path10 = "./combined_breeds_subset_10.dnd"
+file_path11 = "./combined_breeds_subset_11.dnd"
 draw_phylogenetic_tree(file_path1)
 draw_phylogenetic_tree(file_path2)
 draw_phylogenetic_tree(file_path3)
